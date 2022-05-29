14fc00a6cd22714835443bc36a01d56905c8a91a
Kiran Nasim <kiran.cse7@gmail.com>
2022-05-29 20:46:34 +0900

    auth reponsive complete
    phone responsive completed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Topbar from '../../components/Topbar'
-import Banner from '../../components/Banner'
-import Feature from '../../components/Feature'
-import Node from '../../components/Node'
-import Footer from '../../components/Footer'
+import React from "react";
+import Topbar from "../../components/Topbar";
+import Banner from "../../components/Banner";
+import Feature from "../../components/Feature";
+import Node from "../../components/Node";
+import Footer from "../../components/Footer";
 
-import '../../assets/scss/home/home.scss'
+import "../../assets/scss/home/home.scss";
 const Home = () => {
   return (
     <div className="home-page">
@@ -15,7 +15,7 @@ const Home = () => {
       <Node />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
